b17aeac16e1e2185e6bef7a47f4923e680b99290
leejisup <dlwltjq5876@gmail.com>
2022-09-27 13:34:55 +0900

    debug

--- yolov5/utils/metrics.py
+++ yolov5/utils/metrics.py
@@ -87,10 +87,7 @@ def ap_per_class(tp, conf, pred_cls, target_cls, plot=False, save_dir='.', names
         plot_mc_curve(px, r, Path(save_dir) / 'R_curve.png', names, ylabel='Recall')
 
     i = smooth(f1.mean(0), 0.1).argmax()  # max F1 index
-    p, r, f1 = p[:, i], r[:, i], f1[:, i]
-    tp = (r * nt).round()  # true positives
-    fp = (tp / (p + eps) - tp).round()  # false positives
-    return tp, fp, p, r, f1, ap, unique_classes.astype(int)
+    return p[:,i], r[:,i], ap, f1[:,i], unique_classes.astype('int32')
 
 
 def compute_ap(recall, precision):
